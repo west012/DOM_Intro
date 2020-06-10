1fb1b7c880a577ba561d51948d5c96363fec5795
west012 <soul98120@gmail.com>
2020-06-10 12:07:27 -0400

    second_fully_function

--- script.js
+++ script.js
@@ -4,11 +4,20 @@ let body = document.querySelector('body');
 body.setAttribute('style', 'colo: green');
 
 
-let newButton = document.createElement('button');
-body.appendChild(newButton);
-newButton.innerHTML = "ADD";
 
 let i = 0;
+let newButton = document.createElement('button');
+let newButton2 = document.createElement('button');
+
+body.appendChild(newButton);
+body.appendChild(newButton2);
+
+newButton.innerHTML = "ADD";
+newButton2.innerHTML = "REMOVE";
+
+newButton.addEventListener('click', addButtonClicked);
+newButton2.addEventListener('click', removedButtonClicked);
+
 function addButtonClicked(){
     // console.log('hit');
     // alert("Good job finding the button!")
@@ -17,22 +26,26 @@ function addButtonClicked(){
     newHeader.innerHTML ="Jem Necklace";
     newHeader.setAttribute('id',i);
     body.appendChild(newHeader);
-    console.log(newHeader);
+    i++;
+    // console.log(newHeader);
+    
 }
-newButton.addEventListener('click', addButtonClicked);
-
-
-let newButton2 = document.createElement('button');
-body.appendChild(newButton2);
-newButton2.innerHTML = "REMOVE";
-
 function removedButtonClicked(){
-    // console.log('hit');
-    // alert("Good job finding the button!")
+    let toDelete = document.getElementById(i-1);
 
-    let newHeader = document.createElement('h2');
-    newHeader.innerHTML ="Jem Necklace";
-    body.appendChild(newHeader);
+    if(toDelete !== null){
+        console.log(toDelete);
+
+    
+        // alert("Good job finding the button!")
+        // Find variable  with id
+        body.removeChild(toDelete);
+        // id goes back to i-1 after deleting child node
+        i--;
+    }else{
+        window.alert("Don't be daft.")
+    }
+    
 }
-newButton2.addEventListener('click', removedButtonClicked);
-console.log(body);
+
+// console.log(body);
